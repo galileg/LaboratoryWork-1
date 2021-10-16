63052daf582a0794c410e763a0048b755231caf6
galileg <80705966+galileg@users.noreply.github.com>
2021-10-16 17:53:48 +0500

    Final variant

--- LaboratoryWork№1/Program.cs
+++ LaboratoryWork№1/Program.cs
@@ -8,65 +8,29 @@ namespace LaboratoryWork_1
 {
     class Program
     {
-        static double GetAnswer(double a, double b)
-        {
-            double answer = (Math.Pow((a - b), 3) - Math.Pow(a, 3))/(3*a*Math.Pow(b,2)- Math.Pow(b, 3)-3*b*Math.Pow(a,2));
-            return answer;
-        }
-        static void GetMandN(ref int m, ref int n) 
+        
+        static void GetMandN(ref int n) 
         {
             bool OK;
             OK = false;
             do
             {
-                Console.Write("Введите m: \t");
-                OK = int.TryParse(Console.ReadLine(), out m);
-                if (!OK)
-                    Console.WriteLine("Невозможно вычислить m");
-            } while (!OK);
-            OK = false;
-            do
-            {
-                Console.Write("Введите n: \t");
+                Console.Write("Введите число: \t");
                 OK = int.TryParse(Console.ReadLine(), out n);
                 if (!OK)
-                    Console.WriteLine("Невозможно вычислить n");
+                    Console.WriteLine("Невозможно вычислить ");
             } while (!OK);
         }
-        static void GetXandY(ref double x, ref double y)
-        {
-            bool OK;
-            OK = false;
-            do
-            {
-                Console.Write("Введите первую координату: \t");
-                OK = double.TryParse(Console.ReadLine(), out x);
-            } while (!OK);
-            OK = false;
-            do
-            {
-                Console.Write("Введите вторую координату: \t");
-                OK = double.TryParse(Console.ReadLine(), out y);
-            } while (!OK);
-        }
-        static void ContainFigure(double x, double y)
-        {
-            double hipo = Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
-            if (hipo <= 1)
-            { 
-                Console.WriteLine("Точка принадлежит фигуре");
-                if (hipo == 1)
-                    Console.WriteLine("Точка находится на границе");
-            }
-            else
-                Console.WriteLine("Точка не принадлежит фигуре");
-        }
+  
+        
         static void Main(string[] args)
         {
             //FIRST PART
             int n = 0, m = 0;
-            GetMandN(ref m, ref n);
-            int r1 = n++ * m;
+            GetMandN(ref n);
+            GetMandN(ref m);
+            
+            int r1 = m++ * n;
             Console.Write("1)      ");
             Console.WriteLine("n = " + n +"; m = "+m+"\n\tm ++* n = "+r1+"\n\n");
 
@@ -83,28 +47,17 @@ namespace LaboratoryWork_1
             double x;
             do
             {
-                Console.Write("Введите x (>=0): \t");
+                Console.Write("Введите x: \t");
                 OK = double.TryParse(Console.ReadLine(), out x);
                 if (!OK)
                     Console.WriteLine("Невозможно вычислить x");
-            } while (!OK||x<0);
-            double r4;
-            r4 = Math.Pow(x-Math.Pow(x,2)+ Math.Pow(x, 5), 1.0 / 3.0);
+            } while (!OK);
+
+
+            double r4 = Math.Sign(x) * Math.Pow(Math.Abs(x - Math.Pow(x, 2) + Math.Pow(x, 5)), (1.0 / 3.0)); 
+
             Console.WriteLine("\tr4 = " + r4);
 
-            //SECOND PART
-            Console.WriteLine("\n\n\n2 ЧАСТЬ ЗАДАНИЯ");
-            double xCoord = -1, yCoord = -1;
-            GetXandY(ref xCoord, ref yCoord);
-            Console.WriteLine("x = " + xCoord + "\ny = " + yCoord);
-            ContainFigure(xCoord, yCoord);
-
-            //THIRD PART
-            Console.WriteLine("\n\n\n3 ЧАСТЬ ЗАДАНИЯ");
-            double a = 1000; double b = 0.0001;
-            double result = GetAnswer(a, b);
-            Console.WriteLine("Результат тип double = " + result);
-            Console.WriteLine("Результат тип float = " + (float)result);
         }
     }
 }
